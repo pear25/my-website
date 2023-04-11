64ba76208ceeecca9c99eef11e40f0b2b5fcfffa
Pierson Limas <piersonlimas12@gmail.com>
2023-04-12 03:17:21 +0800

    update project page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,15 +1,19 @@
-import moneed from '../assets/moneed.jpeg'
+import { useState } from 'react';
 
-const Project = () => {
+const Project = (props) => {
+
+    const [image, setImage] = useState(props.image);
+
     return (
         <div className="md:w-1/3 w-full rounded-lg">
-            <div className="h-[250px] w-auto border-b-2 border-gray-300">
-                <img src={moneed} className="rounded-t-lg h-[250px] object-cover w-full" />
+            <div className={`h-[250px] w-auto border-b-2 border-gray-300`}>
+                <img src={image} className={props.dark ? `bg-black rounded-t-lg h-[250px] object-contain w-full` :
+                    props.light ? `rounded-t-lg h-[250px] object-cover w-full border-2 border-b-0` : `rounded-t-lg h-[250px] object-cover w-full`} />
             </div>
-            <div className="p-4 border-2 rounded-b-lg border-t-0 space-y-4">
-                <div className="font-semibold text-xl">Moneed</div>
+            <div className={props.light ? `p-4 border-2 rounded-b-lg border-t-0 space-y-4` : `p-4 border-2 rounded-b-lg border-t-0 space-y-4`}>
+                <div className="font-semibold text-xl">{props.title}</div>
                 <div>
-                    Final year project aiming to integrate a money management interface together with a group transaction bill-splitting feature.
+                    {props.description}
                 </div>
                 <div>
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,32 @@
 import Navbar from "./Navbar"
 import moneed from "../assets/moneed.jpeg"
+import hkstp from "../assets/hkstp.jpeg"
 import Project from "./Project"
+import CH from "../assets/ch_logo.png"
+import cat from "../assets/profile.png"
+import gym from "../assets/gym.webp"
 
 const Projects = () => {
     return (
         <>
             <Navbar />
-            <div className="flex md:flex-row flex-col justify-center items-center md:space-x-8 space-y-4 md:space-y-0">
-                <div className="md:w-1/3 w-full rounded-lg">
-                    <div className="h-[250px] w-auto border-b-2 border-gray-300">
-                        <img src={moneed} className="rounded-t-lg h-[250px] object-cover w-full" />
-                    </div>
-                    <div className="p-4 border-2 rounded-b-lg border-t-0 space-y-4">
-                        <div className="font-semibold text-xl">Moneed</div>
-                        <div>
-                            Final year project aiming to integrate a money management interface together with a group transaction bill-splitting feature.
-                        </div>
-                        <div>
-
-                        </div>
-                    </div>
-
+            <div className="md:space-y-8">
+                <div className="flex md:flex-row flex-col justify-center items-center md:space-x-8 space-y-8x md:space-y-0">
+                    <Project image={hkstp} title={'HKSTP Talent Game On'}
+                        description={'Talent Game On 2K23 is a gamification platform for recruitment process in HKSTP, developed as a web game, where users are prompted to solve puzzle games. '} />
+                    <Project image={moneed} title={'Moneed'}
+                        description={'Final year project aiming to integrate a money management interface together with a group transaction bill-splitting feature.'} />
+                    <Project light={true} image={cat} title={'Pearsonal page'}
+                        description={'My current personal page, showcasing some of my projects and more stuff about me. More updates to come soon, stay tuned!!'} />
                 </div>
-                <Project />
+                <div className="flex mt-8 md:mt-0 md:flex-row flex-col justify-center items-center md:space-x-8 space-y-4 md:space-y-0">
+
+
+                    <Project dark={true} image={CH} title={'NFC Business Card'}
+                        description={'Created NFC business cards marketed towards students to expand their network. Reaching 5000 users within 1 week of launch.'} />
+                    <Project image={gym} title={'Upcoming Android Studio project'}
+                        description={'Upcoming project to be finished at May 2023, features a fitness lifestyle app that allows users to track their progress and provide sound recommendations to their fitness goals. '} />
+                </div>
             </div>
         </>
     )
